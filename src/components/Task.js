2e332ef659eee84c081447251fc3426fd49d5e24
Wojciech Paluch <wojciech.paluch@o2.pl>
2018-01-24 13:33:20 +0100

    edited readme

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import Checkbox from 'material-ui/Checkbox'
-import DeleteIcon from 'material-ui-icons/Delete'
+  import React from 'react';
+  import Checkbox from 'material-ui/Checkbox'
+  import DeleteIcon from 'material-ui-icons/Delete'
 
-
-class Task extends React.Component {
+  class Task extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             checked: false
         }
-        this.handleChange1 = this.handleChange1.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange1() {
+    handleChange() {
         this.setState({
             checked: !this.state.checked
         })
     }
 
-        render()
-        {
-            return (
-                <div>
-                    <Checkbox
-                        checked={this.state.checked}
-                        onChange={this.handleChange1}
-                        value="checkedA"
-                    />
-                    {this.props.name}
-                    <DeleteIcon onClick={this.props.onDelete}/>
-                </div>
-            );
-        }
+    render() {
+        return (
+            <div>
+                <Checkbox
+                    checked={this.state.checked}
+                    onChange={this.handleChange}
+                    value="checkedA"
+                />
+                {this.props.name}
+                <DeleteIcon onClick={this.props.onDelete}/>
+            </div>
+        );
     }
+}
 
 
 export default Task;
